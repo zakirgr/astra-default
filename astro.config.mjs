7c5ef14bd3862a8ee77124cdc93909a9a1debcd3
Zajur Sajib <zakir@commandmedia.net>
2023-07-04 16:13:33 +0600

    xxx

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import node from "@astrojs/node";
+
+// https://astro.build/config
+export default defineConfig({
+    output: 'static'
+});
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  })
+});
